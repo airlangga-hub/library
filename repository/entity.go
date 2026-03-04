43880ab59cc07829b2fe6201adc0f604e32ee993
Airlangga <airlanggakrishna@gmail.com>
2026-03-04 08:29:26 +0700

    feat: repo entity

---
+++ repository/entity.go
@@ -0,0 +1,37 @@
+package repository
+
+type User struct {
+	ID       int    `gorm:"primaryKey"`
+	Email    string `gorm:"index"`
+	Password string
+	Balance  int
+	Author   bool
+	Books    []Book `gorm:"foreignKey:AuthorID"`
+	Loans    []Loan `gorm:"foreignKey:UserID"`
+}
+
+type Category struct {
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Books []Book `gorm:"foreignKey:CategoryID"`
+}
+
+type Book struct {
+	ID          int `gorm:"primaryKey"`
+	Title       string
+	Description string
+	AuthorID    int      `gorm:"index"`
+	Author      User     `gorm:"foreignKey:AuthorID"`
+	CategoryID  int      `gorm:"index"`
+	Category    Category `gorm:"foreignKey:CategoryID"`
+	Deposit     int
+	Available   bool
+}
+
+type Loan struct {
+	ID     int  `gorm:"primaryKey"`
+	BookID int  `gorm:"index"`
+	Book   Book `gorm:"foreignKey:BookID"`
+	UserID int  `gorm:"index"`
+	User   User `gorm:"foreignKey:UserID"`
+}
